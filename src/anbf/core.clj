d63cf0dbc79928ed45845b8b1208fb8e8240d768
krajj7 <krajj7@users.noreply.github.com>
2014-03-08 19:39:44 +0100

    pokusy s jta knihovnou

--- src/anbf/core.clj
+++ src/anbf/core.clj
@@ -1,4 +1,31 @@
-(ns anbf.core)
+(ns anbf.core
+  (:import (de.mud.jta PluginLoader PluginConfig))
+  (:import (de.mud.jta.event SocketRequest OnlineStatusListener))
+  (:import (de.mud.telnet TelnetWrapper))
+  (:import (java.util Vector))
+  (:gen-class))
+
+(defn- telnetWrapperTest []
+  (let [t (TelnetWrapper.)
+        buf (byte-array (1024))]
+    (.connect t "nethack.alt.org", 23)
+    ;(.connect t "rainmaker.wunderground.com" 3000)
+    (let [numbytes (.read t buf)]
+        [(str (String. buf 0 numbytes)) numbytes])))
 
 (defn -main [& args] []
-  (println "Hello, World!"))
+  ;                                 cesty kde hleda pluginy
+  (let [pl (PluginLoader. (Vector. ["de.mud.jta.plugin" "anbf"]))]
+    (doto pl
+      (.addPlugin "Socket" "socket")
+      (.addPlugin "Telnet" "telnet")
+      ;(.addPlugin "NethackTerminal" "nethackterm")
+      )
+    ;(println (.getPlugins pl))
+    (.registerPluginListener pl (reify OnlineStatusListener
+                                  (online [_] (println "online"))
+                                  (offline [_] (println "offline"))))
+    (.broadcast pl (SocketRequest. "nethack.alt.org" 23)) ; connect
+    (Thread/sleep 1500)
+    (.broadcast pl (SocketRequest.)) ; disconnect
+    ))
