65401b51ff236c79198f56c9bb7fcd16ced54d6f
krajj7 <krajj7@users.noreply.github.com>
2014-03-16 23:37:43 +0100

    force compilation of the JTA plugin

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :resource-paths ["jta26/jar/jta26.jar"]
   ;:global-vars {*warn-on-reflection* true}
+  :aot [anbf.term] ; force compilation of the JTA plugin
   :main anbf.main)
