1a559b1b337ee8306e454f1841acbab92d5885b9
krajj7 <krajj7@users.noreply.github.com>
2014-03-25 23:44:12 +0100

    log4j

--- project.clj
+++ project.clj
@@ -2,7 +2,13 @@
   :description "A Nethack Bot Framework"
   :url "https://github.com/krajj7/ANBF"
   :license {:name "GPLv2"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.flatland/ordered "1.5.2"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]]
   :resource-paths ["jta26/jar/jta26.jar"]
   ;:global-vars {*warn-on-reflection* true}
   :aot [anbf.term] ; force compilation of the JTA plugin
