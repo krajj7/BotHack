79483ac8c0b5e59942a10767a1cf0a853ba9777d
krajj7 <krajj7@users.noreply.github.com>
2014-03-30 00:13:36 +0100

    reduced boilerplate in event/command protocol definitions

--- src/anbf/delegator.clj
+++ src/anbf/delegator.clj
@@ -4,66 +4,7 @@
   (:require [flatland.ordered.set :refer [ordered-set]]
             [clojure.tools.logging :as log]))
 
-; event protocols
-
-(defprotocol ConnectionStatusHandler
-  (online [handler])
-  (offline [handler]))
-
-(defprotocol RedrawHandler
-  (redraw [handler frame]))
-
-(defprotocol GameStateHandler
-  (started [handler])
-  (ended [handler]))
-
-; end event protocols
-
-; command protocols
-
-; TODO game action requests, prompt/menu reponders
-
-; end command protocols
-
-(defn- invoke-handler [protocol method handler & args]
-  (if (satisfies? protocol handler)
-    (try
-      (apply method handler args)
-      (catch Exception e
-        (log/error e "Delegator caught handler exception")))))
-
-(defn- invoke-event [lockobj protocol method handlers & args]
-  "Events are propagated to all handlers satisfying the protocol in the order of their registration."
-  (locking lockobj
-    (doall (map #(apply invoke-handler protocol method % args)
-                handlers))))
-
-(defn- invoke-command [lockobj protocol method handlers & args]
-  "Commands are propagated to handlers in reverse order and only up to the first handler that satisfies the protocol and returns a non-null value."
-  (locking lockobj
-    ; TODO rseq
-    (throw (UnsupportedOperationException. "not implemented yet"))))
-
-; TODO macro?
-(defrecord Delegator [handlers lockobj]
-  ConnectionStatusHandler
-  (online [_]
-    (invoke-event
-      lockobj ConnectionStatusHandler online handlers))
-  (offline [_]
-    (invoke-event
-      lockobj ConnectionStatusHandler offline handlers))
-  RedrawHandler
-  (redraw [_ frame]
-    (invoke-event
-      lockobj RedrawHandler redraw handlers frame))
-  GameStateHandler
-  (started [_]
-    (invoke-event
-      lockobj GameStateHandler started handlers))
-  (ended [_]
-    (invoke-event
-      lockobj GameStateHandler ended handlers)))
+(defrecord Delegator [handlers lockobj])
 
 (defn new-delegator []
   (Delegator. (ordered-set) (Object.)))
@@ -75,3 +16,59 @@
 (defn deregister [delegator handler]
   (Delegator. (disj (:handlers delegator) handler)
               (:lockobj delegator)))
+
+(defn- invoke-handler [protocol method handler & args]
+  (if (satisfies? protocol handler)
+    (try
+      (apply method handler args)
+      (catch Exception e
+        (log/error e "Delegator caught handler exception")))))
+
+(defn- invoke-event [protocol method handlers & args]
+  "Events are propagated to all handlers satisfying the protocol in the order of their registration."
+  (doall (map #(apply invoke-handler protocol method % args)
+              handlers)))
+
+(defn- invoke-command [lockobj protocol method handlers & args]
+  "Commands are propagated to handlers in reverse order and only up to the first handler that satisfies the protocol and returns a non-null value."
+  ; TODO rseq
+  (throw (UnsupportedOperationException. "not implemented yet")))
+
+(defn- delegation-impl [invoke-fn protocol [method [delegator & args]]]
+  `(~method [~delegator ~@args]
+            (locking (:lockobj ~delegator)
+              (~invoke-fn ~protocol ~method (:handlers ~delegator) ~@args))))
+
+(defmacro ^:private defprotocol-delegated
+  [invoke-fn protocol & proto-methods]
+  `(do (defprotocol ~protocol ~@proto-methods)
+       (extend-type Delegator ~protocol
+         ~@(map (partial delegation-impl invoke-fn protocol)
+                proto-methods))))
+
+(defmacro ^:private defeventhandler [protocol & proto-methods]
+  `(defprotocol-delegated invoke-event ~protocol ~@proto-methods))
+
+(defmacro ^:private defcommandhandler [protocol & proto-methods]
+  `(defprotocol-delegated invoke-command ~protocol ~@proto-methods))
+
+; event protocols:
+
+(defeventhandler ConnectionStatusHandler
+  (online [handler])
+  (offline [handler]))
+
+(defeventhandler ConnectionStatusHandler
+  (online [handler])
+  (offline [handler]))
+
+(defeventhandler RedrawHandler
+  (redraw [handler frame]))
+
+(defeventhandler GameStateHandler
+  (started [handler])
+  (ended [handler]))
+
+; command protocols:
+
+; TODO game action requests, prompt/menu reponders
