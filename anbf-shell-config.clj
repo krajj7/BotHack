4af87546e6e9a7069c19d6dfe0545e208a1ff5e7
krajj7 <krajj7@users.noreply.github.com>
2014-04-13 13:41:04 +0200

    split configuration for interface modes

---
+++ anbf-shell-config.clj
@@ -0,0 +1,5 @@
+{
+ :bot "anbf.bot.sambot"
+ :interface :shell
+ :nh-command "/nh343/nethack.343-nao"
+}
--- anbf-telnet-config.clj
+++ anbf-telnet-config.clj
@@ -1,12 +1,9 @@
 {
- ;:interface :shell
+ :bot "anbf.bot.sambot"
  :interface :telnet
  :menubot "anbf.bot.nao-menu"
- ; for remote (telnet) game
  :host "nethack.alt.org"
  :port 23
  :nao-login "quitbot"
  :nao-pass "tops3cr3t"
- ; for local game
- :nh-command "/nh343/nethack.343-nao"
 }
