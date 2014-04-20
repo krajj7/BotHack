009dfc7e38f1d754d08d9e3c5045362a61f8cc3e
krajj7 <krajj7@users.noreply.github.com>
2014-04-21 00:11:43 +0200

    make the bot pray for food

--- src/anbf/bot/sambot.clj
+++ src/anbf/bot/sambot.clj
@@ -4,18 +4,30 @@
   (:require [anbf.util :refer :all]
             [anbf.delegator :refer :all]
             [anbf.action :refer :all]
+            [anbf.player :refer :all]
             [clojure.tools.logging :as log]))
 
+(def ^:private circle-large (cycle [1 4 7 8 9 6 3 2]))
+
+(def ^:private circle-small (cycle [6 2 4 8]))
+
 (defn- circle-mover []
-  (let [circle (atom (cycle [1 4 7 8 9 6 3 2]))]
+  (let [circle (atom circle-small)]
     (reify ActionHandler
-      (choose-action [this anbf]
+      (choose-action [_ _]
         (->Move (first (swap! circle next)))))))
 
+(defn- pray-if-weak []
+  (reify ActionHandler
+    (choose-action [_ game]
+      (if (weak? (:player game))
+        (->Pray)))))
+
 (defn init [anbf]
-  (register-handler anbf
-                    (reify ChooseCharacterHandler
-                      (choose-character [this]
-                        (deregister-handler anbf this)   
-                        "nsm")))
-  (register-handler anbf (circle-mover)))
+  (-> anbf
+      (register-handler (reify ChooseCharacterHandler
+                          (choose-character [this]
+                            (deregister-handler anbf this)
+                            "nsm")))
+      (register-handler (circle-mover))
+      (register-handler (pray-if-weak))))
