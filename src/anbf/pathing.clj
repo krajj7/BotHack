c41c2abf3522fadbb0d6f3a47dbe899d8f047890
krajj7 <krajj7@users.noreply.github.com>
2014-05-28 19:03:24 +0200

    move position-related logic

--- src/anbf/pathing.clj
+++ src/anbf/pathing.clj
@@ -3,55 +3,8 @@
             [clojure.tools.logging :as log]
             [anbf.position :refer :all]
             [anbf.delegator :refer :all]
-            [anbf.action :refer :all]
+            [anbf.actions :refer :all]
             [anbf.dungeon :refer :all]))
-
-(def <=> (comp #(Integer/signum %) compare))
-
-(def directions
-  {[-1  1] :NW [0  1] :N [1  1] :NE
-   [-1  0] :W            [1  0] :E
-   [-1 -1] :SW [0 -1] :S [1 -1] :SE})
-
-(def straight #{:N :W :S :E})
-(def diagonal #{:NW :SW :NE :SE})
-
-(defn towards [from to]
-  (get directions ((juxt #(<=> (:x %2) (:x %1))
-                         #(<=> (:y %1) (:y %2)))
-                   from to)))
-
-(defn diagonal? [from to]
-  (diagonal (towards from to)))
-
-(defn straight? [from to]
-  (straight (towards from to)))
-
-(defn diagonal-walkable? [tile]
-  (if (not-any? #(= % (:feature tile)) [nil :door-open])
-    true)) ;TODO no diagonal near boulders, squeeze-spaces
-
-(defn passable-walking?
-  "Only needs Move action, no door opening etc."
-  [level from to]
-  (let [from-tile (at level from)
-        to-tile (at level to)]
-    (and (or (walkable? to-tile)
-             (and (not (boulder? to-tile))
-                  (= nil (:feature to-tile)))) ; try to path via unexplored tiles
-         (or (straight (towards from to))
-             (and (diagonal-walkable? from-tile)
-                  (diagonal-walkable? to-tile))))))
-
-(defn passable-travelling?
-  "Consider opening/breaking doors" ; TODO unlocking shops, digging
-  [level from to]
-    (or (passable-walking? level from to)
-        (let [from-tile (at level from)
-              to-tile (at level to)]
-          (and (door? to-tile)
-               (or (not= :door-locked (:feature to-tile))
-                   (not (:shop to-tile)))))))
 
 (defn move-cost [level from to]
   ;(cond->)
@@ -59,24 +12,14 @@
   ; diagonal closed door +1 (kick)
   0) ; TODO
 
-(defn neighbors
-  ([level tile]
-   (map #(at level %) (neighbors tile)))
-  ([pos]
-   (filter valid-position?
-           (map #(hash-map :x (+ (:x pos) (% 0))
-                           :y (+ (:y pos) (% 1)))
-                (keys directions)))))
-
-(defn distance [from to]
-  (max (Math/abs (- (:x from) (:x to)))
-       (Math/abs (- (:y from) (:y to)))))
+(defn diagonal-walkable? [tile]
+  (if (not-any? #(= % (:feature tile)) [nil :door-open])
+    true)) ;TODO no diagonal near boulders, squeeze-spaces
 
 (defn a* [from to passable? extra-cost]
   "Extra-cost must always return non-negative values, target tile may not be passable, but will always be included in the path"
   (loop [closed {}
          open (priority-map-keyfn first (position from) [0 0])]
-    ;(log/debug (count open))
     (if (empty? open) nil
       (let [[node [total dist prev]] (peek open)
             path (conj (closed prev []) node)
@@ -118,3 +61,91 @@
                                            node]])))))))))
 
 (def nearest dijkstra)
+
+; XXX stuff below will need a redesign but will do for now
+
+(defn passable-walking?
+  "Only needs Move action, no door opening etc."
+  [level from to]
+  (let [from-tile (at level from)
+        to-tile (at level to)]
+    (and (or (walkable? to-tile)
+             (and (not (boulder? to-tile))
+                  (= nil (:feature to-tile)))) ; try to path via unexplored tiles
+         (or (straight (towards from to))
+             (and (diagonal-walkable? from-tile)
+                  (diagonal-walkable? to-tile))))))
+
+(defn passable-travelling?
+  "Consider opening/breaking doors" ; TODO unlocking shops, digging
+  [level from to]
+    (or (passable-walking? level from to)
+        (let [from-tile (at level from)
+              to-tile (at level to)]
+          (and (door? to-tile)
+               (or (not= :door-locked (:feature to-tile))
+                   (not (:shop to-tile)))))))
+
+(defn nearest-walking [game goal?]
+  (let [level (curlvl (:dungeon game))]
+    (nearest (-> game :player)
+             #(goal? (at level %))
+             (partial passable-walking? level)
+             (partial move-cost level))))
+
+(defn nearest-travelling [game goal?]
+  (let [level (curlvl (:dungeon game))]
+    (nearest (-> game :player)
+             #(goal? (at level %))
+             (partial passable-travelling? level)
+             (partial move-cost level))))
+
+(defn path-walking [game to]
+  (let [level (curlvl (:dungeon game))]
+    (path (-> game :player) to
+          (partial passable-walking? level)
+          (partial move-cost level))))
+
+(defn path-travelling [game to]
+  (let [level (curlvl (:dungeon game))]
+    (path (-> game :player) to
+          (partial passable-travelling? level)
+          (partial move-cost level))))
+
+(defn move-travelling [{:keys [dungeon player] :as game} path]
+  (if (passable-walking? (curlvl dungeon) player (first path))
+    (->Move (towards player (first path)))
+    ; TODO should look ahead if we have to move diagonally FROM the door in the next step and kick door down if necessary
+    (let [to-tile (at (curlvl dungeon) (first path))
+          dir (towards player to-tile)]
+      (if (door? to-tile)
+        (if (diagonal dir)
+          (if (= :door-open (:feature to-tile))
+            (->Close dir)
+            (->Kick dir))
+          (if (= :door-locked (:feature to-tile))
+            (->Kick dir)
+            (->Open dir)))))))
+
+(defn travel [to]
+  (reify ActionHandler
+    (choose-action [this game]
+      (if-let [p (path-travelling game to)]
+        (if (empty? p)
+          (log/debug "reached travel target")
+          (or (move-travelling game p)
+              (log/debug "failed to move according to path")))
+        (do (log/debug "travel target not pathable")
+            (Thread/sleep 200)))))) ; XXX
+
+(defn walk [to]
+  (reify ActionHandler
+    (choose-action [this game]
+      (if-let [p (path-walking game to)]
+        (if-let [n (first p)]
+          (if (passable-walking? (-> game :dungeon curlvl) (-> game :player) n)
+            (->Move (towards (-> game :player) n))
+            (log/debug "walk reached unwalkable target"))
+          (log/debug "reached walk target"))
+        (do (log/debug "walk target not pathable")
+            (Thread/sleep 200)))))) ; XXX
--- src/anbf/position.clj
+++ src/anbf/position.clj
@@ -1,6 +1,7 @@
 (ns anbf.position)
 
 (defn position [of]
+  ; TODO IPosition for java
   (select-keys of [:x :y]))
 
 (defn valid-position? [{:keys [x y]}]
@@ -12,6 +13,45 @@
   {:pre [(valid-position? pos)]}
   (-> level :tiles (nth (dec y)) (nth x)))
 
+(def directions [:NW :N :NE :W :E :SW :S :SE])
+(def deltas [[-1  -1] [0  -1] [1  -1] [-1  0] [1  0] [-1 1] [0 1] [1 1]])
+(def dirmap (merge (zipmap directions deltas)
+                   (zipmap deltas directions)))
+
+(def straight #{:N :W :S :E})
+(def diagonal #{:NW :SW :NE :SE})
+
+(def <=> (comp #(Integer/signum %) compare))
+
 (defn adjacent? [pos1 pos2]
   (and (<= (Math/abs (- (:x pos1) (:x pos2))) 1)
        (<= (Math/abs (- (:y pos1) (:y pos2))) 1)))
+
+(defn neighbors
+  ([level tile]
+   (map #(at level %) (neighbors tile)))
+  ([pos]
+   (filter valid-position?
+           (map #(hash-map :x (+ (:x pos) (% 0))
+                           :y (+ (:y pos) (% 1))) deltas))))
+
+(defn in-direction [from dir]
+  {:pre [(valid-position? from)]}
+  (assoc (position from)
+         :x (+ ((dirmap dir) 0) (:x from))
+         :y (+ ((dirmap dir) 1) (:y from))))
+
+(defn towards [from to]
+  (get dirmap ((juxt #(<=> (:x %2) (:x %1))
+                     #(<=> (:y %2) (:y %1)))
+               from to)))
+
+(defn diagonal? [from to]
+  (diagonal (towards from to)))
+
+(defn straight? [from to]
+  (straight (towards from to)))
+
+(defn distance [from to]
+  (max (Math/abs (- (:x from) (:x to)))
+       (Math/abs (- (:y from) (:y to)))))
