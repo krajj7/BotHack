8459c4b00da0130252ae573236cfa073ff73f62e
krajj7 <krajj7@users.noreply.github.com>
2014-06-12 18:16:44 +0200

    tracker fixup + memory stub

--- src/anbf/tracker.clj
+++ src/anbf/tracker.clj
@@ -5,35 +5,45 @@
             [anbf.dungeon :refer :all]
             [clojure.tools.logging :as log]))
 
-(defn- track-transfer [game old-monster monster]
+(defn- transfer-pair [game [old-monster monster]]
   (log/debug "transfer:" \newline old-monster "to" \newline monster)
   (update-curlvl-monster game monster into ; TODO type
                          (select-keys old-monster [:peaceful :cancelled])))
 
+(defn- transfer-unpaired [game unpaired]
+  (log/debug "unpaired" unpaired) ; TODO
+  game
+  #_(if (not (in-los? (at-curlvl game unpaired)))
+    (update-curlvl-monster game unpaired (constantly unpaired))
+    game))
+
 (defn track-monsters
-  "Try to transfer monster properties from the old game snapshot to the new, even if the monsters moved slightly."
+  "Try to transfer monster properties greedily from the old game snapshot to the new, even if the monsters moved slightly."
   [new-game old-game]
   (if (not= (-> old-game :dungeon :dlvl)
             (-> new-game :dungeon :dlvl))
     new-game ; TODO track stair followers?
-    (loop [res new-game
+    (loop [pairs {}
            new-monsters (curlvl-monsters new-game)
            old-monsters (curlvl-monsters old-game)
            dist 0]
-      (if (> 3 dist)
+      (if (and (> 3 dist) (seq old-monsters))
         (if-let [[p m] (first new-monsters)]
-          (if-let [candidates (seq (->> (vals old-monsters)
-                                        (filter
-                                          (fn candidate? [n]
-                                            (and (= (:glyph m) (:glyph n))
-                                                 (= (:color m) (:color n))
-                                                 (= dist (distance m n)))))))]
-            (if (next candidates) ; ignore ambiguous cases
-              (recur res (dissoc new-monsters p) old-monsters dist)
-              (recur (track-transfer res (first candidates) m)
+          (if-let [[[cp cm] & more] (seq (filter
+                                           (fn candidate? [[_ n]]
+                                             (and (= (:glyph m) (:glyph n))
+                                                  (= (:color m) (:color n))
+                                                  (= dist (distance m n))))
+                                           old-monsters))]
+            (if more ; ignore ambiguous cases
+              (recur pairs (dissoc new-monsters p) old-monsters dist)
+              (recur (assoc pairs p [cm m])
                      (dissoc new-monsters p)
-                     (dissoc old-monsters (position (first candidates)))
+                     (dissoc old-monsters cp)
                      dist))
-            (recur res (dissoc new-monsters p) old-monsters dist))
-          (recur res new-monsters old-monsters (inc dist)))
-        res)))) ; TODO remember/unremember unignored old/new
+            (recur pairs (dissoc new-monsters p) old-monsters dist))
+          (recur pairs (apply dissoc (curlvl-monsters new-game) (keys pairs))
+                 old-monsters (inc dist)))
+        (as-> new-game res
+          (reduce transfer-pair res (vals pairs))
+          (reduce transfer-unpaired res old-monsters))))))
