e5f1a6df1290beb1f881e0841cd715cedf777e6b
krajj7 <krajj7@users.noreply.github.com>
2014-12-16 21:54:14 +0100

    handle single-digit str

--- src/anbf/util.clj
+++ src/anbf/util.clj
@@ -172,7 +172,7 @@
   (filterv (complement pred) coll))
 
 (defn effective-str [s]
-  (cond (= 2 (.length s)) (parse-int s)
+  (cond (>= 2 (.length s)) (parse-int s)
         (.endsWith s "**") 21
         (< 49 (parse-int (subs s 3))) 19
         :else 20))
