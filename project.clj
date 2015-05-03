8877c0f434c108d70687801463ce551f2e00bf00
krajj7 <krajj7@users.noreply.github.com>
2015-05-03 21:25:10 +0200

    use more recent ssh lib

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
                  [com.cemerick/pomegranate "0.3.0"]
                  [org.flatland/ordered "1.5.2"]
                  [org.clojars.achim/multiset "0.1.0-SNAPSHOT"]
-                 [com.jcraft/jsch "0.1.42"]
+                 [com.jcraft/jsch "0.1.52"]
                  [criterium "0.4.3"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
